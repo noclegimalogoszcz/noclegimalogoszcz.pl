ae3e83345d4509a50d949d1573e28020db0be361
noclegimalogoszcz <tiq7@o2.pl>
2025-05-21 23:02:26 +0200

    Add files via upload

---
+++ language-switch.js
@@ -0,0 +1,9 @@
+
+function switchLanguage(lang) {
+  fetch(lang + '.json')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('title').textContent = data.title;
+      document.getElementById('desc').textContent = data.desc;
+    });
+}
