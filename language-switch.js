c2fc6a50bd6cedb73fda85070b63ff061edeaa7c
noclegimalogoszcz <tiq7@o2.pl>
2025-05-21 23:09:37 +0200

    Delete language-switch.js

--- language-switch.js
+++
@@ -1,9 +0,0 @@
-
-function switchLanguage(lang) {
-  fetch(lang + '.json')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('title').textContent = data.title;
-      document.getElementById('desc').textContent = data.desc;
-    });
-}
